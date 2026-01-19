5fbf9d43c713cb9250c3aeb1c132ac7e34b5f4de
MDSagarALI762 <mdsagarali762@gmail.com>
2026-01-19 17:33:58 +0600

    Add files via upload

---
+++ db - Copy.js
@@ -0,0 +1,8 @@
+const posts = require("./database/posts.json");
+const users = require("./database/users.json");
+const contacts = require("./database/contacts.json");
+module.exports = {
+  posts,
+  users,
+  contacts,
+};
